e15a946ea1f5911e7d4f8675d9066001034dabac
rwedzony <rwedzony@gmail.com>
2020-08-25 11:08:40 +0200

    added Venv, created main game file with  2 paddles and ball

--- src/main.py
+++ src/main.py
@@ -1,1 +1,97 @@
-print("Hello world!!!")
+import turtle
+import time
+
+wn = turtle.Screen()
+wn.title("Pong by Rafwedz")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+
+wn.tracer(0)
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("red")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+# print("paddle_a size"+paddle_a.x)
+
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx=2
+ball.dy=-2
+
+
+#Functions
+def paddle_a_up():
+    y=paddle_a.ycor()
+    y+=20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y=paddle_a.ycor()
+    y-=20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+
+
+while True:
+    wn.update();
+    time.sleep(0.01)
+   # ball.setx(ball.xcor()+ball.dx)
+   # ball.sety(ball.ycor()+ball.dy)
+
+    #borderchecking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx*=-1
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx*=-1
+
+    #Paddle and ball collisions
+    #Paddle a collisions
